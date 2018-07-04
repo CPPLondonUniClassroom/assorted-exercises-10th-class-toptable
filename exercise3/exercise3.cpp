d808d3aca8728c4107dc92067a0574812f21b050
Lewis Cowles <lewiscowles@me.com>
2018-07-04 09:41:43 +0100

    Completed exercise 3

--- exercise3/exercise3.cpp
+++ exercise3/exercise3.cpp
@@ -1,6 +1,25 @@
-
+#include <string>
 #include "exercise3.hpp"
 
+std::vector<std::string> split_by(const char c, const std::string& str)
+{
+      std::vector<std::string> out = {};
+      std::string tmp = "";
+      auto pos = 0;
+      while(str[pos] != 0) {
+          if(str[pos] != c) {
+              tmp += str[pos];
+          } else {
+              out.push_back(std::string(tmp));
+              tmp = "";
+          }
+          pos++;
+      }
+      if(tmp != "") {
+        out.push_back(std::string(tmp));
+      }
+      return out;
+}
 // Requirements:
 //   -- Given a string containing elements separated by commas, return a
 //      vector of strings containing each element
@@ -8,7 +27,7 @@
 //    split("a,b,c") -> vector<string>("a", "b", "c");
 std::vector<std::string> split(const std::string& str)
 {
-    return {};
+    return split_by(',', str);
 }
 
 // An element which is enclosed in double quotes should be treated as a single element,
@@ -21,7 +40,25 @@ std::vector<std::string> split(const std::string& str)
 //      elements as a single unit
 std::vector<std::string> quoted_split(const std::string& str)
 {
-    return {};
+    std::vector<std::string> out = {};
+    std::string tmp = "";
+    auto pos = 0;
+    auto in_dblquotes = false;
+    while(str[pos] != 0) {
+        if(str[pos] == '"') {
+          in_dblquotes = !in_dblquotes;
+        } else if((str[pos] != ',') || in_dblquotes) {
+            tmp += str[pos];
+        } else {
+            out.push_back(std::string(tmp));
+            tmp = "";
+        }
+        pos++;
+    }
+    if(tmp != "") {
+      out.push_back(std::string(tmp));
+    }
+    return out;
 }
 
 // A CSV file consists of multiple lines, each of which should be split
@@ -29,5 +66,10 @@ std::vector<std::string> quoted_split(const std::string& str)
 // a `csv_file` containing each line of the input, split correctly
 csv_file parse_csv(std::istream& istream)
 {
-    return {};
+    std::string line = "";
+    csv_file out = {};
+    while(std::getline(istream, line)) {
+        out.lines.push_back(quoted_split(line));
+    }
+    return out;
 }
